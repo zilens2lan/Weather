7706f5a809025646862cbd3198c74b672aed8584
ZilenS2Lan <zilens2lan@gmail.com>
2021-06-30 20:39:30 +0300

    Work on bugs.

--- src/main/java/com/zilen/weather/entity/MainFactorsDTO.java
+++ src/main/java/com/zilen/weather/entity/MainFactorsDTO.java
@@ -1,6 +1,5 @@
 package com.zilen.weather.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
